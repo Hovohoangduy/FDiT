fdfcf34c7bb93e61b27a04295501db4c8fc264f5
duyho <h.hoangduy2002@gmail.com>
2025-09-12 14:01:33 +0700

    updates parameters gentron

--- DM/train_vfd_mhad.py
+++ DM/train_vfd_mhad.py
@@ -19,7 +19,7 @@ from DM.modules.vfdm_with_gentron import FlowDiffusionGenTron
 from torch.optim.lr_scheduler import MultiStepLR
 
 start = timeit.default_timer()
-BATCH_SIZE = 1
+BATCH_SIZE = 2
 MAX_EPOCH = 1200
 epoch_milestones = [800, 1000]
 root_dir = 'log'
@@ -58,11 +58,11 @@ SAMPLE_VID_EVERY = 2000
 UPDATE_MODEL_EVERY = 500
 
 ### parameters with gentron
-MODEL_DIM = 128
-MODEL_DEPTH = 4
-MODEL_HEADS = 2
-MODEL_DIM_HEAD = 32
-MODEL_MLP_DIM = 256
+MODEL_DIM = 256
+MODEL_DEPTH = 12
+MODEL_HEADS = 4
+MODEL_DIM_HEAD = 128
+MODEL_MLP_DIM = 512
 DIFF_TIMESTEPS = 1000 
 DDIM_ETA = 0.0  
 ADAM_BETAS = (0.9, 0.999)
